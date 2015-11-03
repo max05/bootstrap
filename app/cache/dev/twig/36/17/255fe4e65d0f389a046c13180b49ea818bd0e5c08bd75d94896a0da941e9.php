da2101c0b38cdf2d34a37febc4d1fb7e4641daf9
maxime <max.poulmane@gmail.com>
2015-11-03 16:28:51 +0100

    new

---
+++ app/cache/dev/twig/36/17/255fe4e65d0f389a046c13180b49ea818bd0e5c08bd75d94896a0da941e9.php
@@ -0,0 +1,49 @@
+<?php
+
+/* PPEHopitalBundle:Default:services.html.twig */
+class __TwigTemplate_3617255fe4e65d0f389a046c13180b49ea818bd0e5c08bd75d94896a0da941e9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("PPEHopitalBundle:Default:base.html.twig");
+
+        $this->blocks = array(
+            'corps' => array($this, 'block_corps'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "PPEHopitalBundle:Default:base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_corps($context, array $blocks = array())
+    {
+        // line 3
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "PPEHopitalBundle:Default:services.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 3,  28 => 2,);
+    }
+}
---
+++ app/cache/dev/twig/41/b0/6b66ce1a92e8f6836e054b8f50c2aa446e838f3c6d5bd84477be8f02818c.php
@@ -0,0 +1,49 @@
+<?php
+
+/* PPEHopitalBundle:Default:services.html.twig */
+class __TwigTemplate_41b06b66ce1a92e8f6836e054b8f50c2aa446e838f3c6d5bd84477be8f02818c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("PPEHopitalBundle:Default:base.html.twig");
+
+        $this->blocks = array(
+            'corps' => array($this, 'block_corps'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "PPEHopitalBundle:Default:base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_corps($context, array $blocks = array())
+    {
+        // line 3
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "PPEHopitalBundle:Default:services.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 3,  28 => 2,);
+    }
+}
